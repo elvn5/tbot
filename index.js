0e7b8ff661638f1a875ac4a66c802a4274086fcd
elvn5 <a.ermek.kg@gmail.com>
2021-02-01 23:48:16 +0600

    new commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'TOKEN'
+const token = '1407013955:AAHg0hrT6w8KkXVhvhDOA3itnWDraFrL3y0'
 var port = process.env.PORT || 8443;
 var host = process.env.HOST;
 var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
 bot.startPolling()
+const mainMenu = {
+  "reply_markup": {
+      "keyboard": [["Химия", "Физика"],   ["Алгебра", "Геометрия"]]
+      }
+  }
 let books = {
   fizika8: 'BQACAgIAAxkBAAEIqSdgDw64qc5gA7vTXmxLDUVoKSUAAfMAAtEJAAJ0bnlINTqtfxgfxhUeBA',
   fizika9: 'BQACAgIAAxkBAAEIqSZgDw64h9X-xyHq1Iz4D6PAhhrTEAAC0AkAAnRueUhCyHEgx7GsrR4E',
@@ -12,21 +17,23 @@ let books = {
   chemistry8: 'BQACAgIAAxkBAAEIqTVgDxBPLgHhQoQ0s6ACyvas5iTETwAC2QkAAnRueUi0ZtrSExkELh4E',
   chemistry9: 'BQACAgIAAxkBAAEIlVRgDaOAWpUSOUPIbbIK-zufeAtKSwAC0gkAAlvCkUp8MIneR7LSBB4E',
   chemistry10: 'BQACAgIAAxkBAAEIqStgDw64KOsES1hw7YPep0Rgw8Aq-AAC1wkAAnRueUgwJr3FaxT5kx4E',
-  chemistry11: 'BQACAgIAAxkBAAEIqSpgDw64clOhvFSxmA3Uh-6DpQaC3QAC1AkAAnRueUjCpjA8Fjgmth4E'
+  chemistry11: 'BQACAgIAAxkBAAEIqSpgDw64clOhvFSxmA3Uh-6DpQaC3QAC1AkAAnRueUjCpjA8Fjgmth4E',
+  algebra7: 'BQACAgIAAxkBAAEIxlRgGDCnSBiIlWEQ6X-XZoL748P6yQAC3woAAswAAclIHswwPTvvqcQeBA',
+  algebra8: 'BQACAgIAAxkBAAEIxl1gGDGt_wABMnt9g2ojzkapDY8SsLQAAuYKAALMAAHJSDvzJL34qIXHHgQ',
+  algebra9: 'BQACAgIAAxkBAAEIxlhgGDGtoDAzLyI7RK_SmRX9SuS9pQAC4QoAAswAAclIjJrR3cno_MAeBA',
+  algebra10_11: 'BQACAgIAAxkBAAEIxmxgGDJ3GCWaovGcRS0AAS4CWivQtdIAAuIKAALMAAHJSK57hBYEU6ePHgQ',
+  geometry10_11: 'BQACAgIAAxkBAAEIxm5gGDK9SbqJFQAB4wRs8D6i121hHowAAuMKAALMAAHJSMn38cHOkvWsHgQ',
+  geometry7_9: 'BQACAgIAAxkBAAEIxm9gGDK9dvudIIwPs_fI8tF1HgnangAC5AoAAswAAclIsa6X5z5RKpMeBA'
+
 }
 
-bot.on('sticker', msg => bot.sendMessage(msg.chat.id, 'Роботам стикеры не нужны'))
 
 bot.on('message', (msg) => {
-  //anything
+  bot.sendMessage(434413828, `${msg.text}, ${msg.chat.first_name}, ${msg.chat.last_name}, ${msg.chat.username}`)
 });
 bot.on("polling_error", (err) => console.log(err));
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Добро пожаловать, я могу отправить книгу по нужному", {
-  "reply_markup": {
-      "keyboard": [["Химия"],   ["Физика"]]
-      }
-  });
+  bot.sendMessage(msg.chat.id, "Добро пожаловать, я могу отправить книгу по нужному предмету", mainMenu);
       
   });
   
@@ -37,6 +44,22 @@ bot.onText(/\/start/, (msg) => {
           }
       });
   
+});
+
+bot.onText(/^Геометрия$/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Выберите класс", {
+    "reply_markup": {
+        "keyboard": [["Геометрия 7-9 Класс"], ["Геометрия 10-11 Класс"], ["Главное меню"]]
+        }
+    });
+});
+
+bot.onText(/^Алгебра$/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Выберите класс", {
+    "reply_markup": {
+        "keyboard": [["Алгебра 7 Класс"], ["Алгебра 8 Класс"], ["Алгебра 9 Класс"], ["Алгебра 10-11 Класс"], ["Главное меню"]]
+        }
+    });
 });
 
 bot.onText(/^Физика$/, (msg) => {
@@ -90,11 +113,39 @@ bot.onText(/^Физика 11 Класс$/,  (msg) => {
   bot.sendDocument(msg.chat.id, books.fizika11)
 });
 
+bot.onText(/^Алгебра 7 Класс$/,  (msg) => {
+
+  bot.sendDocument(msg.chat.id, books.algebra7)
+});
+
+bot.onText(/^Алгебра 8 Класс$/,  (msg) => {
+
+  bot.sendDocument(msg.chat.id, books.algebra8)
+});
+
+bot.onText(/^Алгебра 9 Класс$/,  (msg) => {
+
+  bot.sendDocument(msg.chat.id, books.algebra9)
+});
+
+bot.onText(/^Алгебра 10-11 Класс$/,  (msg) => {
+
+  bot.sendDocument(msg.chat.id, books.algebra10_11)
+});
+
+bot.onText(/^Геометрия 7-9 Класс$/,  (msg) => {
+
+  bot.sendDocument(msg.chat.id, books.geometry7_9)
+});
+
+bot.onText(/^Геометрия 10-11 Класс$/,  (msg) => {
+
+  bot.sendDocument(msg.chat.id, books.geometry10_11)
+});
+
+
+
 bot.onText(/^Главное меню$/,  (msg) => {
-  bot.sendMessage(msg.chat.id, "Вы в главном меню", {
-    "reply_markup": {
-        "keyboard": [["Химия"],   ["Физика"]]
-        }
-    });
+  bot.sendMessage(msg.chat.id, "Вы в главном меню", mainMenu);
 });
 
